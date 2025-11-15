3a3fb833010d91fcb4eb97015566f4215d680476
Balwinder Katoch <vickykatoch@yahoo.com>
2025-11-15 11:45:23 -0500

    changes

--- apps/view-hub/src/App.tsx
+++ apps/view-hub/src/App.tsx
@@ -1,20 +1,30 @@
 import { Calculator } from '@avam/eog-client';
 import { useState } from 'react';
 import { NumberSpinner } from '@avam/components';
+import { SaltProviderNext } from '@salt-ds/core';
 
 const calculator = new Calculator();
 export const App = () => {
    const [value, setValue] = useState(0);
    return (
-      <div>
-         <NumberSpinner />
-         <span>{value}</span>
-         <button
-            data-testid="add-button"
-            onClick={() => setValue((value) => calculator.add(value, Math.random() * 100))}
-         >
-            ADD
-         </button>
-      </div>
+      <SaltProviderNext
+         mode="dark"
+         applyClassesTo="root"
+         actionFont="Amplitude"
+         headingFont="Amplitude"
+         corner="rounded"
+         accent="teal"
+      >
+         <div>
+            <NumberSpinner />
+            <span>{value}</span>
+            <button
+               data-testid="add-button"
+               onClick={() => setValue((value) => calculator.add(value, Math.random() * 100))}
+            >
+               ADD
+            </button>
+         </div>
+      </SaltProviderNext>
    );
 };
--- apps/view-hub/src/main.tsx
+++ apps/view-hub/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import '@salt-ds/theme/css/theme-next.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
    <React.StrictMode>
